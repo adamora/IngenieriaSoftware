8ae9bd943ebd1cc463250c69faf97f9512c2cdb5
Adam <i02motoa@uco.es>
2015-08-21 18:37:26 +0200

    Casi finalizadas las operaciones referentes a ficheros

---
+++ Agenda/src/SGDB.cpp
@@ -0,0 +1,12 @@
+/*
+ * SGDB.cpp
+ *
+ *  Created on: 21 de ago. de 2015
+ *      Author: adam
+ */
+
+#include "SGDB.h"
+
+namespace Dentista{
+
+}
---
+++ Agenda/src/SGDBFichero.cpp
@@ -0,0 +1,12 @@
+/*
+ * SGDBFichero.cpp
+ *
+ *  Created on: 21 de ago. de 2015
+ *      Author: adam
+ */
+
+#include "SGDBFichero.h"
+
+namespace Dentista{
+
+}
